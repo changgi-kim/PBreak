8965cd53bd5c016c41aaf40d6d4dc097d464ec3a
cgkim <cgkim@nhn.com>
2013-01-22 15:13:43 +0900

    asdf

--- PBreak/PBAppDelegate.h
+++ PBreak/PBAppDelegate.h
@@ -1,10 +1,11 @@
-//
-//  PBAppDelegate.h
-//  PBreak
-//
-//  Created by cgkim on 13. 1. 22..
-//  Copyright (c) 2013년 cgkim. All rights reserved.
-//
+/*
+ *  PBAppDelegate.h
+ *  PBreak
+ *
+ *  Created by cgkim on 13. 1. 22..
+ *  Copyright (c) 2013년 cgkim. All rights reserved.
+ *
+ */
 
 #import <UIKit/UIKit.h>
 
