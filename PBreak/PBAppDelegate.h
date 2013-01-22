6b64ff5e5a2b20d7c0383a51fabb4b646f8fdb36
cgkim <cgkim@nhn.com>
2013-01-22 14:16:08 +0900

    Initial Commit

---
+++ PBreak/PBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PBAppDelegate.h
+//  PBreak
+//
+//  Created by cgkim on 13. 1. 22..
+//  Copyright (c) 2013년 cgkim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PBreak/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PBreak
+//
+//  Created by cgkim on 13. 1. 22..
+//  Copyright (c) 2013년 cgkim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PBAppDelegate class]));
+    }
+}
